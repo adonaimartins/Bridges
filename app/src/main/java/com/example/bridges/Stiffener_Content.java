528b99ce0768c35ba720689bf86c7f19f3233fa5
adonai <andreg325@hotmail.com>
2021-01-26 11:03:59 +0000

    bays nearly done, stiffener, stiff content, we, web defects and web lobby done or nearly done

--- app/src/main/java/com/example/bridges/Stiffener_Content.java
+++ app/src/main/java/com/example/bridges/Stiffener_Content.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BaySingle extends AppCompatActivity {
+public class Stiffener_Content extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bay_single);
+        setContentView(R.layout.activity_stiffener__content);
     }
 }
